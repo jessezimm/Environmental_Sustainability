9511dee75c18f5f9712e345a5ee460fe3b0454c0
BBright07 <89029747+BBright07@users.noreply.github.com>
2022-01-20 18:58:34 -0500

    finalized the model and improved accuracy

--- Database/WaterData_TableJoin.sql
+++ Database/WaterData_TableJoin.sql
@@ -1,6 +1,4 @@
---- Creating tables for water-potability database
---create water table 1
-create table waterdata1 (
+create table water_data1 (
 id INT PRIMARY key,
 	ph float,
 	Hardness float,
@@ -8,34 +6,31 @@ id INT PRIMARY key,
 	Chloramines float,
 	Sulfate float);
 
-	--create water table 2
-create table waterdata2 (
-	id INT PRIMARY key REFERENCES waterdata1 (id),
+create table water_data2 (
+	id INT PRIMARY key REFERENCES water_data1 (id),
 	Conductivity float,
 	Organic_carbon float,
 	Trihalomethanes float,
 	Turbidity float,
 	Potability int)
 
-select * from dbo.waterdata1;
-select * from dbo.waterdata2;
+select * from dbo.water_data1;
+select * from dbo.water_data2;
 
-select waterdata1.id,
-waterdata1.ph,
-waterdata1.Hardness,
-waterdata1.Solids,
-waterdata1.Chloramines,
-waterdata1.Sulfate,
-waterdata2.Conductivity,
-waterdata2.Organic_carbon,
-waterdata2.Trihalomethanes,
-waterdata2.Turbidity,
-waterdata2.Potability
-INTO cleanwaterdata
-FROM waterdata1
-LEFT JOIN waterdata2
-ON waterdata1.id = waterdata2.id;
+select water_data1.id,
+water_data1.ph,
+water_data1.Hardness,
+water_data1.Solids,
+water_data1.Chloramines,
+water_data1.Sulfate,
+water_data2.Conductivity,
+water_data2.Organic_carbon,
+water_data2.Trihalomethanes,
+water_data2.Turbidity,
+water_data2.Potability
+INTO joinedwaterdata
+FROM water_data1
+LEFT JOIN water_data2
+ON water_data1.id = water_data2.id;
 
-select * from cleanwaterdata
-
-
+select * from joinedwaterdata
